240060dfd6942a4e63d97375a96b1a5ff614a239
spencercap <cappiello.spencer@gmail.com>
2021-04-08 14:12:43 +0200

    modularized lib so chronology extends gpt-ts lib

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
+import { GpTs } from 'gpt-ts';
+
 export * from './utils';
-import { AnswerRequest, ClassificationRequest, CompletionRequest, EngineId, SearchRequest } from './typings';
+// import { AnswerRequest, ClassificationRequest, CompletionRequest, EngineId, SearchRequest } from './typings';
 
 // in case this is not the web import fetch
 import fetch from 'node-fetch';
 
-export class Chronology {
-	hello = 'world';
-	apiKey: string;
+export class Chronology extends GpTs {
+	// hello = 'world';
 
 	constructor(apiKey: string) {
-		// console.log('Chronolgy constructed');
-		this.apiKey = apiKey;
+		super(apiKey);
 	}
 
 	async listEngines(): Promise<any> {
@@ -21,78 +21,5 @@ export class Chronology {
 		});
 	}
 
-	async retrieveEngine(engineId: EngineId): Promise<any> {
-		return await fetch(`https://api.openai.com/v1/engines/${engineId}`, {
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`
-			}
-		});
-	}
-
-	async createCompletion(engineId: EngineId, options: CompletionRequest): Promise<any> {
-		return await fetch(`https://api.openai.com/v1/engines/${engineId}/completions`, {
-			body: JSON.stringify(options),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
-			method: 'POST'
-		});
-	}
-
-	// TODO: https://beta.openai.com/docs/api-reference/completions/create-via-get
-	async createCompletionStream(engineId: EngineId, options: Partial<CompletionRequest>): Promise<any> {
-		console.log('TODO');
-		return;
-	}
-
-	async createSearch(engineId: EngineId, options: SearchRequest): Promise<any> {
-		return await fetch(`https://api.openai.com/v1/engines/${engineId}/search`, {
-			body: JSON.stringify(options),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
-			method: 'POST'
-		});
-	}
-
-	async createClassification(engineId: EngineId, options: ClassificationRequest): Promise<any> {
-		const bod = {
-			model: engineId,
-			...options
-		};
-		return await fetch('https://api.openai.com/v1/classifications', {
-			body: JSON.stringify(bod),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
-			method: 'POST'
-		});
-	}
-
-	// async createAnswer(engineId: EngineId, options: Partial<AnswerRequest>): Promise<any> {
-	// 	const bod = {
-	// 		model: engineId,
-	// 		...options
-	// 	} as AnswerRequest;
-	async createAnswer(engineId: EngineId, options: AnswerRequest): Promise<any> {
-		const bod = {
-			model: engineId,
-			...options
-		};
-		return await fetch('https://api.openai.com/v1/answers', {
-			body: JSON.stringify(bod),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
-			method: 'POST'
-		});
-	}
-
-	// TODO files
-	// https://beta.openai.com/docs/api-reference/files/list
 }
 export default Chronology;
